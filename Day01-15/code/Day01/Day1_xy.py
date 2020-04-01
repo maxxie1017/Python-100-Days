de699fa06a49815a69bc3fd98c5c7774b3001cf8
xuyun <xuyun921017@163.com>
2020-04-01 22:41:26 +0800

    test git with Day1_xy.py

---
+++ Day01-15/code/Day01/Day1_xy.py
@@ -0,0 +1,15 @@
+import this
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('black')
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.mainloop()
