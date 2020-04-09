71055a96d4291c29e4a0c614b65e1be8539dff4d
XU Yun <xuyun3@shanghai-electric.com>
2020-04-09 17:15:54 +0800

    update Day8

--- Day01-15/code/Day08/Day8_xy.py
+++ Day01-15/code/Day08/Day8_xy.py
@@ -5,7 +5,7 @@ class Student(object):
     def __init__(self, name, age, gender):
         self.name = name
         self.age = age
-        self.__gender = gender
+        self._gender = gender
 
     def __bar(self):
         print(self.__gender)
@@ -20,9 +20,71 @@ class Student(object):
         if self.age < 18:
             print('%s只能观看《熊出没》.' % self.name)
         else:
-            print('%s正在观看岛国爱情大电影.' % self.name)
+            print('%s正在观看大电影.' % self.name)
 
-me = Student('xuyun',27, 'female')
-me.watch_movie()
-print(me.__dict__)
-print(me._Student__gender)
+
+class Clock(object):
+    def __init__(self, hour=0, minute=0, second=0):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+
+    def run(self):
+        self.second += 1
+        if self.second == 60:
+            self.minute += 1
+            self.second = 0
+            if self.minute == 60:
+                self.hour += 1
+                self.minute = 0
+                if self.hour == 60:
+                    self.hour = 0
+
+    def showtime(self):
+        string = 'Now the time is %02d:%02d:%02d' % (self.hour, self.minute, self.second)
+        print(string)
+        return string
+
+
+def main_clock():
+    import time
+    clock = Clock(12, 23, 45)
+    while True:
+        clock.run()
+        clock.showtime()
+        time.sleep(1)
+
+class Point(object):
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def move(self, dx=0, dy=0):
+        self.x += dx
+        self.y += dy
+
+    def caldistance(self, other):
+        from math import sqrt
+        d = sqrt((self.x - other.x) ** 2 + (self.y - other.y) ** 2)
+        return d
+
+
+def distance(p1, p2):
+    from math import sqrt
+    d = sqrt((p1.x-p2.x)**2+(p1.y-p2.y)**2)
+    return d
+
+
+def main():
+    p1 = Point()
+    p2 = Point(1, 3)
+    p1.move(2, 5)
+    d = distance(p1, p2)
+    d2 = p1.caldistance(p2)
+    print('distance between point (%s, %s) and (%s, %s) is %.2f' %(p1.x, p1.y, p2.x, p2.y, d2))
+
+
+if __name__ == '__main__':
+    main()
+
