52463de41b8c389083e5b7ee47563b4c61506e3c
xuyun <xuyun921017@163.com>
2020-05-08 20:33:04 +0800

    upload my files in May 8th from surface

---
+++ Day01-15/code/Day13/Day13_xy.py
@@ -0,0 +1,202 @@
+"""
+进程：Process，进程就是操作系统中执行的一个程序
+fork或spawn创建
+进程具有独立的内存空间，需要通过Inter-Process Communication共享数据
+共享方式：管道、信号、套接字、共享内存区
+
+线程：一个进程还可以拥有多个并发的执行线索，简单的说就是拥有多个可以获得CPU调度的执行单元
+共享信息更加容易
+"""
+
+# Windows系统用multiprocessing实现多进程
+
+from multiprocessing import Process, Queue
+from os import getpid
+from random import randint
+from time import time, sleep
+from threading import Thread, Lock
+import tkinter
+import tkinter.messagebox
+
+
+def download_task(filename):
+    print('启动下载进程，进程号[%d].' % getpid())
+    print('开始下载%s...' % filename)
+    time_to_download = randint(5, 10)
+    sleep(time_to_download)
+    print('%s下载完成! 耗费了%d秒' % (filename, time_to_download))
+
+
+def main_multiprocessing():
+    start = time()
+    p1 = Process(target=download_task, args=('Python从入门到住院.pdf', ))
+    p1.start()
+    p2 = Process(target=download_task, args=('Peking Hot.avi', ))
+    p2.start()
+    p1.join()  # join:等待进程执行结束
+    p2.join()
+    end = time()
+    print('总共耗费了%.2f秒.' % (end - start))
+
+
+def download(filename):
+    print('开始下载%s...' % filename)
+    time_to_download = randint(5, 10)
+    sleep(time_to_download)
+    print('%s下载完成! 耗费了%d秒' % (filename, time_to_download))
+
+
+def main_thread():
+    start = time()
+    t1 = Thread(target=download, args=('Python从入门到住院.pdf',))
+    t1.start()
+    t2 = Thread(target=download, args=('Peking Hot.avi',))
+    t2.start()
+    t1.join()
+    t2.join()
+    end = time()
+    print('总共耗费了%.3f秒' % (end - start))
+
+
+class DownloadTask(Thread):  # 自定义新线程类
+
+    def __init__(self, filename):
+        super().__init__()
+        self._filename = filename
+
+    def run(self):  # Thread本身有run函数，子类override
+        print('开始下载%s...' % self._filename)
+        time_to_download = randint(5, 10)
+        sleep(time_to_download)
+        print('%s下载完成! 耗费了%d秒' % (self._filename, time_to_download))
+
+
+def main_downloadtask():
+    start = time()
+    t1 = DownloadTask('Python从入门到住院.pdf')
+    t1.start()
+    t2 = DownloadTask('Peking Hot.avi')
+    t2.start()
+    t1.join()
+    t2.join()
+    end = time()
+    print('总共耗费了%.2f秒.' % (end - start))
+
+
+class Account(object):
+
+    def __init__(self):
+        self._balance = 0
+        self._lock = Lock()
+
+    def deposit(self, money):
+        # 计算存款后的余额
+        self._lock.acquire()
+        try:
+            new_balance = self._balance + money
+            # 模拟受理存款业务需要0.01秒的时间
+            sleep(0.01)
+             # 修改账户余额
+            self._balance = new_balance
+        finally:
+            self._lock.release()
+
+    @property
+    def balance(self):
+        return self._balance
+
+
+class AddMoneyThread(Thread):
+
+    def __init__(self, account, money):
+        super().__init__()
+        self._account = account
+        self._money = money
+
+    def run(self):
+        self._account.deposit(self._money)
+
+
+def main_lock():
+    account = Account()
+    threads = []
+    # 创建100个存款的线程向同一个账户中存钱
+    for _ in range(100):
+        t = AddMoneyThread(account, 1)
+        threads.append(t)
+        t.start()
+    # 等所有存款的线程都执行完毕
+    for t in threads:
+        t.join()
+    print('账户余额为: ￥%d元' % account.balance)
+
+
+def main_thread():
+
+    class DownloadTaskHandler(Thread):
+
+        def run(self):
+            sleep(10)
+            tkinter.messagebox.showinfo('提示', '下载完成!')
+            # 启用下载按钮
+            button1.config(state=tkinter.NORMAL)
+
+    def download():
+        # 禁用下载按钮
+        button1.config(state=tkinter.DISABLED)
+        # 通过daemon参数将线程设置为守护线程(主程序退出就不再保留执行)
+        # 在线程中处理耗时间的下载任务
+        DownloadTaskHandler(daemon=True).start()
+
+    def show_about():
+        tkinter.messagebox.showinfo('关于', '作者: 骆昊(v1.0)')
+
+    top = tkinter.Tk()
+    top.title('单线程')
+    top.geometry('200x150')
+    top.wm_attributes('-topmost', 1)
+
+    panel = tkinter.Frame(top)
+    button1 = tkinter.Button(panel, text='下载', command=download)
+    button1.pack(side='left')
+    button2 = tkinter.Button(panel, text='关于', command=show_about)
+    button2.pack(side='right')
+    panel.pack(side='bottom')
+
+    tkinter.mainloop()
+
+
+def task_handler(curr_list, result_queue):
+    total = 0
+    for number in curr_list:
+        total += number
+    result_queue.put(total)
+
+
+def main():
+    processes = []
+    number_list = [x for x in range(1, 100000001)]
+    result_queue = Queue()
+    index = 0
+    # 启动8个进程将数据切片后进行运算
+    for _ in range(8):
+        p = Process(target=task_handler,
+                    args=(number_list[index:index + 12500000], result_queue))
+        index += 12500000
+        processes.append(p)
+        p.start()
+    # 开始记录所有进程执行完成花费的时间
+    start = time()
+    for p in processes:
+        p.join()
+    # 合并执行结果
+    total = 0
+    while not result_queue.empty():
+        total += result_queue.get()
+    print(total)
+    end = time()
+    print('Execution time: ', (end - start), 's', sep='')
+
+
+if __name__ == '__main__':
+    main()
---
+++ Day01-15/code/Day14/Day14_xy.py
@@ -0,0 +1,98 @@
+from time import time
+from threading import Thread
+from socket import socket, SOCK_STREAM, AF_INET
+from datetime import datetime
+import requests
+
+
+# 继承Thread类创建自定义的线程类
+class DownloadHanlder(Thread):
+
+    def __init__(self, url):
+        super().__init__()
+        self.url = url
+
+    def run(self):
+        filename = self.url[self.url.rfind('/') + 1:]
+        resp = requests.get(self.url)
+        # 下方不能用相对路径？
+        with open('G:/Python/Python-100-Days/Day01-15/code/Day14/' + filename, 'wb') as f:
+            f.write(resp.content)
+
+
+def main_requests():
+    # 通过requests模块的get函数获取网络资源
+    # 下面的代码中使用了天行数据接口提供的网络API
+    # 要使用该数据接口需要在天行数据的网站上注册
+    # 然后用自己的Key替换掉下面代码的中APIKey即可
+    resp = requests.get(
+        'http://api.tianapi.com/meinv/?key=127a71fce1f098aa632976161005ac8c&area=上海')
+    # 将服务器返回的JSON格式的数据解析为字典
+    data_model = resp.json()
+    for mm_dict in data_model['newslist']:
+        url = mm_dict['picUrl']
+        # 通过多线程的方式实现图片下载
+        DownloadHanlder(url).start()
+
+
+def main_socket():
+    # 1.创建套接字对象并指定使用哪种传输服务
+    # family=AF_INET - IPv4地址
+    # family=AF_INET6 - IPv6地址
+    # type=SOCK_STREAM - TCP套接字
+    # type=SOCK_DGRAM - UDP套接字
+    # type=SOCK_RAW - 原始套接字
+    server = socket(family=AF_INET, type=SOCK_STREAM)
+    # 2.绑定IP地址和端口(端口用于区分不同的服务)
+    # 同一时间在同一个端口上只能绑定一个服务否则报错
+    server.bind(('127.0.0.1', 6789))
+    # 3.开启监听 - 监听客户端连接到服务器
+    # 参数512可以理解为连接队列的大小
+    server.listen(512)
+    print('服务器启动开始监听...')
+    while True:
+        # 4.通过循环接收客户端的连接并作出相应的处理(提供服务)
+        # accept方法是一个阻塞方法如果没有客户端连接到服务器代码不会向下执行
+        # accept方法返回一个元组其中的第一个元素是客户端对象
+        # 第二个元素是连接到服务器的客户端的地址(由IP和端口两部分构成)
+        client, addr = server.accept()
+        print(str(addr) + '连接到了服务器.')
+        # 5.发送数据
+        client.send(str(datetime.now()).encode('utf-8'))
+        # 6.断开连接
+        client.close()
+
+
+def main_socket():
+    # 1.创建套接字对象默认使用IPv4和TCP协议
+    client = socket()
+    # 2.连接到服务器(需要指定IP地址和端口)
+    client.connect(('127.0.0.1', 6789))
+    # 3.从服务器接收数据
+    print(client.recv(1024).decode('utf-8'))
+    client.close()
+
+
+from smtplib import SMTP
+from email.header import Header
+from email.mime.text import MIMEText
+
+
+def main():
+    # 请自行修改下面的邮件发送者和接收者
+    sender = 'xuyun921017@163.com'
+    receivers = ['xuyun921017@163.com']
+    message = MIMEText('用Python发送邮件的示例代码.', 'plain', 'utf-8')
+    message['From'] = Header('王大锤', 'utf-8')
+    message['To'] = Header('骆昊', 'utf-8')
+    message['Subject'] = Header('示例代码实验邮件', 'utf-8')
+    smtper = SMTP('smtp.126.com')
+    # 请自行修改下面的登录口令
+    smtper.login(sender, 'wangyi94264983')
+    smtper.sendmail(sender, receivers, message.as_string())
+    print('邮件发送完成!')
+
+
+
+if __name__ == '__main__':
+    main()
